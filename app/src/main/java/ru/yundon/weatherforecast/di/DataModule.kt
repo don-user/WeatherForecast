61004d144094486f04bdd97344ba419204b22b48
don-user <don_1303@mail.ru>
2022-03-31 13:20:58 +0300

    api_work_through_di delete CitiesWeatherApiClient.kt add in DataModule.kt new provides change RemoteDataSource.kt

--- app/src/main/java/ru/yundon/weatherforecast/di/DataModule.kt
+++ app/src/main/java/ru/yundon/weatherforecast/di/DataModule.kt
@@ -6,17 +6,38 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import ru.yundon.weatherforecast.data.CityWeatherMapper
+import ru.yundon.weatherforecast.data.api.ApiCitiesWeather
+import ru.yundon.weatherforecast.data.api.RemoteDataSource
 import ru.yundon.weatherforecast.data.database.CitiesWeatherDatabase
 import ru.yundon.weatherforecast.data.datarepository.CitiesWeatherLocalStorage
 import ru.yundon.weatherforecast.data.datarepository.CitiesWeatherRemoteStorage
 import ru.yundon.weatherforecast.data.datarepository.CitiesWeatherRepositoryImpl
 import ru.yundon.weatherforecast.domain.CitiesWeatherRepository
+import ru.yundon.weatherforecast.utils.Constants.BASE_URL
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DataModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): ApiCitiesWeather {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit.create(ApiCitiesWeather::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDataSource(service: ApiCitiesWeather) : RemoteDataSource{
+        return RemoteDataSource(service)
+    }
 
     @Provides
     @Singleton
@@ -32,8 +53,11 @@ class DataModule {
 
     @Provides
     @Singleton
-    fun provideRemoveStorage(mapper: CityWeatherMapper) : CitiesWeatherRemoteStorage {
-        return CitiesWeatherRemoteStorage(mapper)
+    fun provideRemoveStorage(
+        remoteDataSource: RemoteDataSource,
+        mapper: CityWeatherMapper
+    ) : CitiesWeatherRemoteStorage {
+        return CitiesWeatherRemoteStorage(remoteDataSource, mapper)
     }
 
     @Provides
